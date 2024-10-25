11d3e9de78db8924568071f1bd49b141c1d8f0c4
gufir <gunawan.firdaus99@ymail.com>
2024-10-25 20:23:52 +0700

    refactor(config): get DSN from .env

--- back-end/config/config.go
+++ back-end/config/config.go
@@ -1,12 +1,21 @@
 package config
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=admin password=admin123 dbname=mydatabase port=5432 sslmode=disable"
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error getting env, %v", err)
+	}
+	dsn := os.Getenv("DSN")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
